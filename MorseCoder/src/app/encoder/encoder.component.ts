e06484dd0a73f14bfaa730954a16a548052b25da
HathalerMichael <m.hathaler@students.htl-leonding.ac.at>
2024-10-04 09:04:59 +0200

    Implemented Player

--- MorseCoder/src/app/encoder/encoder.component.ts
+++ MorseCoder/src/app/encoder/encoder.component.ts
@@ -1,6 +1,7 @@
 import { Component, input, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { of } from 'rxjs';
+import { initializeAudioContext, LETTER_BREAK, playDash, playDot, sleep, SYMBOL_BREAK } from './player';
 
 @Component({
   selector: 'app-encoder',
@@ -12,6 +13,7 @@ import { of } from 'rxjs';
 export class EncoderComponent {
   inputText : string  = '';
   resultStr = signal('');
+  resultText : string[] = [];
 
   morseCode : string[] = [
     /* A */ '.-',
@@ -43,7 +45,6 @@ export class EncoderComponent {
   ];
 
   encode() : void{
-    let resultText : string[] = [];
 
     for (let i = 0; i < this.inputText.length; i++){
       let letter = this.inputText[i].toUpperCase();
@@ -51,26 +52,43 @@ export class EncoderComponent {
 
       if (charCode >= 65 && charCode <= 90){
         let index = letter.charCodeAt(0) - 65;
-        resultText.push(this.morseCode[index]);
+        this.resultText.push(this.morseCode[index]);
       }
       else if (letter === ' '){
         if (i !== 0){
           if (this.inputText[i - 1] !== ' '){
-            resultText.push('/');
+            this.resultText.push('/');
         }}}
       else{
         throw new Error('Invalid character: ' + letter);
       }
 
     }
-    console.log(resultText);
+    console.log(this.resultText);
 
     this.resultStr.set('');
     let tmp : string = '';
-    resultText.forEach((element) => {
-
+    this.resultText.forEach((element) => {
       tmp += element + ' ';
     });
     this.resultStr.set(tmp);
+  }
+
+  async playMorseCode() : Promise<void>{
+
+    initializeAudioContext();
+    for (let i = 0; i < this.resultText.length; i++){
+      let symbol = this.resultText[i];
+      for (let j = 0; j < symbol.length; j++){
+        if (symbol[j] === '.'){
+          await playDot();
+        }
+        else if (symbol[j] === '-'){
+          await playDash();
+        }
+        await sleep(SYMBOL_BREAK);
+      }
+      await sleep(LETTER_BREAK);
+    }
   }
 }
